dc276065c13a5a8f3ab9a189edce286bd92eb2c2
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2024-11-07 03:52:31 +0200

    Modify pour not to change predicates in the KB and implement IDS

--- Watersort.pl
+++ Watersort.pl
@@ -2,269 +2,160 @@
 :- dynamic bottle1/2, bottle2/2, bottle3/2.
 :- dynamic result/2.
 
-
 % ************************************************************ Pour Operations ************************************************************
 
-
-
 % Pour from bottle1 to bottle2
-pour(1, 2) :-
-    bottle1(Top1, Bottom1),
-    bottle2(Top2, Bottom2),
+pour(1, 2, state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), B3), NewState) :-
     (
         % Case 1: [Top1, Bottom1] -> [e, Bottom2]
         (Top1 \= e, Top2 = e, Bottom2 \= e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle1(e, Bottom1)),
-            assert(bottle2(Top1, Bottom2))
+            NewState = state(bottle1(e, Bottom1), bottle2(Top1, Bottom2), B3)
         )
         ;
         % Case 2: [Top1, Bottom1] -> [e, e]
         (Top1 \= e, Top2 = e, Bottom2 = e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle1(e, Bottom1)),
-            assert(bottle2(e, Top1))
+            NewState = state(bottle1(e, Bottom1), bottle2(e, Top1), B3)
         )
         ;
         % Case 3: [e, Bottom1] -> [e, Bottom2]
         (Top1 = e, Bottom1 \= e, Top2 = e, Bottom2 \= e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle1(e, e)),
-            assert(bottle2(Bottom1, Bottom2))
+            NewState = state(bottle1(e, e), bottle2(Bottom1, Bottom2), B3)
         )
         ;
         % Case 4: [e, Bottom1] -> [e, e]
         (Top1 = e, Bottom1 \= e, Top2 = e, Bottom2 = e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle1(e, e)),
-            assert(bottle2(e, Bottom1))
+            NewState = state(bottle1(e, e), bottle2(e, Bottom1), B3)
         )
     ).
 
 % Pour from bottle1 to bottle3
-pour(1, 3) :-
-    bottle1(Top1, Bottom1),
-    bottle3(Top3, Bottom3),
+pour(1, 3, state(bottle1(Top1, Bottom1), B2, bottle3(Top3, Bottom3)), NewState) :-
     (
         % Case 1: [Top1, Bottom1] -> [e, Bottom3]
         (Top1 \= e, Top3 = e, Bottom3 \= e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle1(e, Bottom1)),
-            assert(bottle3(Top1, Bottom3))
+            NewState = state(bottle1(e, Bottom1), B2, bottle3(Top1, Bottom3))
         )
         ;
         % Case 2: [Top1, Bottom1] -> [e, e]
         (Top1 \= e, Top3 = e, Bottom3 = e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle1(e, Bottom1)),
-            assert(bottle3(e, Top1))
+            NewState = state(bottle1(e, Bottom1), B2, bottle3(e, Top1))
         )
         ;
         % Case 3: [e, Bottom1] -> [e, Bottom3]
         (Top1 = e, Bottom1 \= e, Top3 = e, Bottom3 \= e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle1(e, e)),
-            assert(bottle3(Bottom1, Bottom3))
+            NewState = state(bottle1(e, e), B2, bottle3(Bottom1, Bottom3))
         )
         ;
         % Case 4: [e, Bottom1] -> [e, e]
-        (Top1 = e, Bottom1 \= e,
-            retract(bottle1(Top1, Bottom1)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle1(e, e)),
-            assert(bottle3(e, Bottom1))
+        (Top1 = e, Bottom1 \= e, Top3 = e, Bottom3 = e,
+            NewState = state(bottle1(e, e), B2, bottle3(e, Bottom1))
         )
     ).
 
 % Pour from bottle2 to bottle1
-pour(2, 1) :-
-    bottle2(Top2, Bottom2),
-    bottle1(Top1, Bottom1),
+pour(2, 1, state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), B3), NewState) :-
     (
         % Case 1: [Top2, Bottom2] -> [e, Bottom1]
         (Top2 \= e, Top1 = e, Bottom1 \= e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle2(e, Bottom2)),
-            assert(bottle1(Top2, Bottom1))
+            NewState = state(bottle1(Top2, Bottom1), bottle2(e, Bottom2), B3)
         )
         ;
         % Case 2: [Top2, Bottom2] -> [e, e]
         (Top2 \= e, Top1 = e, Bottom1 = e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle2(e, Bottom2)),
-            assert(bottle1(e, Top2))
+            NewState = state(bottle1(e, Top2), bottle2(e, Bottom2), B3)
         )
         ;
         % Case 3: [e, Bottom2] -> [e, Bottom1]
         (Top2 = e, Bottom2 \= e, Top1 = e, Bottom1 \= e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle2(e, e)),
-            assert(bottle1(Bottom2, Bottom1))
+            NewState = state(bottle1(Bottom2, Bottom1), bottle2(e, e), B3)
         )
         ;
         % Case 4: [e, Bottom2] -> [e, e]
-        (Top2 = e, Bottom2 \= e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle2(e, e)),
-            assert(bottle1(e, Bottom2))
+        (Top2 = e, Bottom2 \= e, Top1 = e, Bottom1 = e,
+            NewState = state(bottle1(e, Bottom2), bottle2(e, e), B3)
         )
     ).
 
 % Pour from bottle2 to bottle3
-pour(2, 3) :-
-    bottle2(Top2, Bottom2),
-    bottle3(Top3, Bottom3),
+pour(2, 3, state(B1, bottle2(Top2, Bottom2), bottle3(Top3, Bottom3)), NewState) :-
     (
         % Case 1: [Top2, Bottom2] -> [e, Bottom3]
         (Top2 \= e, Top3 = e, Bottom3 \= e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle2(e, Bottom2)),
-            assert(bottle3(Top2, Bottom3))
+            NewState = state(B1, bottle2(e, Bottom2), bottle3(Top2, Bottom3))
         )
         ;
         % Case 2: [Top2, Bottom2] -> [e, e]
         (Top2 \= e, Top3 = e, Bottom3 = e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle2(e, Bottom2)),
-            assert(bottle3(e, Top2))
+            NewState = state(B1, bottle2(e, Bottom2), bottle3(e, Top2))
         )
         ;
         % Case 3: [e, Bottom2] -> [e, Bottom3]
         (Top2 = e, Bottom2 \= e, Top3 = e, Bottom3 \= e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle2(e, e)),
-            assert(bottle3(Bottom2, Bottom3))
+            NewState = state(B1, bottle2(e, e), bottle3(Bottom2, Bottom3))
         )
         ;
         % Case 4: [e, Bottom2] -> [e, e]
-        (Top2 = e, Bottom2 \= e,
-            retract(bottle2(Top2, Bottom2)),
-            retract(bottle3(Top3, Bottom3)),
-            assert(bottle2(e, e)),
-            assert(bottle3(e, Bottom2))
+        (Top2 = e, Bottom2 \= e, Top3 = e, Bottom3 = e,
+            NewState = state(B1, bottle2(e, e), bottle3(e, Bottom2))
         )
     ).
 
 % Pour from bottle3 to bottle1
-pour(3, 1) :-
-    bottle3(Top3, Bottom3),
-    bottle1(Top1, Bottom1),
+pour(3, 1, state(bottle1(Top1, Bottom1), B2, bottle3(Top3, Bottom3)), NewState) :-
     (
         % Case 1: [Top3, Bottom3] -> [e, Bottom1]
         (Top3 \= e, Top1 = e, Bottom1 \= e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle3(e, Bottom3)),
-            assert(bottle1(Top3, Bottom1))
+            NewState = state(bottle1(Top3, Bottom1), B2, bottle3(e, Bottom3))
         )
         ;
         % Case 2: [Top3, Bottom3] -> [e, e]
         (Top3 \= e, Top1 = e, Bottom1 = e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle3(e, Bottom3)),
-            assert(bottle1(e, Top3))
+            NewState = state(bottle1(e, Top3), B2, bottle3(e, Bottom3))
         )
         ;
         % Case 3: [e, Bottom3] -> [e, Bottom1]
         (Top3 = e, Bottom3 \= e, Top1 = e, Bottom1 \= e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle3(e, e)),
-            assert(bottle1(Bottom3, Bottom1))
+            NewState = state(bottle1(Bottom3, Bottom1), B2, bottle3(e, e))
         )
         ;
         % Case 4: [e, Bottom3] -> [e, e]
-        (Top3 = e, Bottom3 \= e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle1(Top1, Bottom1)),
-            assert(bottle3(e, e)),
-            assert(bottle1(e, Bottom3))
+        (Top3 = e, Bottom3 \= e, Top1 = e, Bottom1 = e,
+            NewState = state(bottle1(e, Bottom3), B2, bottle3(e, e))
         )
     ).
 
 % Pour from bottle3 to bottle2
-pour(3, 2) :-
-    bottle3(Top3, Bottom3),
-    bottle2(Top2, Bottom2),
+pour(3, 2, state(B1, bottle2(Top2, Bottom2), bottle3(Top3, Bottom3)), NewState) :-
     (
         % Case 1: [Top3, Bottom3] -> [e, Bottom2]
         (Top3 \= e, Top2 = e, Bottom2 \= e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle3(e, Bottom3)),
-            assert(bottle2(Top3, Bottom2))
+            NewState = state(B1, bottle2(Top3, Bottom2), bottle3(e, Bottom3))
         )
         ;
         % Case 2: [Top3, Bottom3] -> [e, e]
         (Top3 \= e, Top2 = e, Bottom2 = e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle3(e, Bottom3)),
-            assert(bottle2(e, Top3))
+            NewState = state(B1, bottle2(e, Top3), bottle3(e, Bottom3))
         )
         ;
         % Case 3: [e, Bottom3] -> [e, Bottom2]
         (Top3 = e, Bottom3 \= e, Top2 = e, Bottom2 \= e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle3(e, e)),
-            assert(bottle2(Bottom3, Bottom2))
+            NewState = state(B1, bottle2(Bottom3, Bottom2), bottle3(e, e))
         )
         ;
         % Case 4: [e, Bottom3] -> [e, e]
-        (Top3 = e, Bottom3 \= e,
-            retract(bottle3(Top3, Bottom3)),
-            retract(bottle2(Top2, Bottom2)),
-            assert(bottle3(e, e)),
-            assert(bottle2(e, Bottom3))
+        (Top3 = e, Bottom3 \= e, Top2 = e, Bottom2 = e,
+            NewState = state(B1, bottle2(e, Bottom3), bottle3(e, e))
         )
     ).
 
 % ************************************************************ Main Logic ************************************************************
 
-is_goal_bottle(bottle1(Top, Bottom)) :-
-    (Top = e, Bottom = e)                
-    ;
-    (Top = e, Bottom \= e)                 
-    ;
-    (Top = Bottom, Top \= e).       
-
-is_goal_bottle(bottle2(Top, Bottom)) :-
-    (Top = e, Bottom = e)                 
-    ;
-    (Top = e, Bottom \= e)                 
-    ;
-    (Top = Bottom, Top \= e).
-
-is_goal_bottle(bottle3(Top, Bottom)) :-
-    (Top = e, Bottom = e)                 
-    ;
-    (Top = e, Bottom \= e)                 
-    ;
-    (Top = Bottom, Top \= e).
-
-
-s0.
-
-s(state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3))) :-
+% Define the initial state from the KB
+initial_state(state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3))) :-
     bottle1(Top1, Bottom1),
     bottle2(Top2, Bottom2),
     bottle3(Top3, Bottom3).
+
 
 is_goal_state(state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3))) :-
     is_goal_bottle(bottle1(Top1, Bottom1)),
@@ -272,7 +163,24 @@ is_goal_state(state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3
     is_goal_bottle(bottle3(Top3, Bottom3)).
 
 
-result(Action, Situation).
+is_goal_bottle(bottle1(Top1, Bottom1)) :-
+    (Top1 = e, Bottom1 \= e)
+    ;
+    (Top1 = Bottom1).
+
+is_goal_bottle(bottle2(Top2, Bottom2)) :-
+    (Top2 = e, Bottom2 \= e)
+    ;
+    (Top2 = Bottom2).
+
+is_goal_bottle(bottle3(Top3, Bottom3)) :-
+    (Top3 = e, Bottom3 \= e)
+    ;
+    (Top3 = Bottom3).
+
+
+apply_pour(pour(From, To), State, NewState) :-
+    pour(From, To, State, NewState).
 
 
 search(State, Situation, S) :-
@@ -281,9 +189,10 @@ search(State, Situation, S) :-
 
 search(State, Situation, S) :-
     pour_operation(Pour),
-    call(Pour),
-    s(NewState),
-    search(NewState, result(Pour, Situation), S).
+    apply_pour(Pour, State, NewState),
+    NewSituation = result(Pour, Situation),
+    search(NewState, NewSituation, S).
+
 
 pour_operation(pour(1, 2)).
 pour_operation(pour(1, 3)).
@@ -294,6 +203,26 @@ pour_operation(pour(3, 2)).
 
 
 goal(S) :-
-    s(InitialState),
-    search(InitialState, s0, S).
+    initial_state(InitialState),
+    ids(InitialState, S).
+
+
+ids(State, S) :-
+    L = 0,
+    ids(State, S, L).
+
+ids(State, S, L) :-
+    call_with_depth_limit(search(State, s0, S), L, R),
+    R \= depth_limit_exceeded.
+
+ids(State, S, L) :-
+    L1 is L + 1,
+    ids(State, S, L1).
+
+
+
+
+
+
+    
     

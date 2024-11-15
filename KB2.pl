e40dc9cf9f568b355aa68f47d284429316a3c9ad
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2024-11-15 09:12:33 +0200

    Implement successor state axiom

--- KB2.pl
+++ KB2.pl
@@ -1,3 +1,3 @@
-bottle1(g,r). % Initial colors of bottle 1 (The top of the bottle is b and the bottom is r)
-bottle2(r,g). % Initial colors of bottle 2 (The top of the bottle is b and the bottom is r)
-bottle3(r,g). % Initial colors of bottle 3 (Empty bottle)
+bottle1(b,r). % Initial colors of bottle 1 (The top of the bottle is b and the bottom is r)
+bottle2(e,r). % Initial colors of bottle 2 (The top of the bottle is b and the bottom is r)
+bottle3(e,b). % Initial colors of bottle 3 (Empty bottle)
--- Watersort.pl
+++ Watersort.pl
@@ -1,12 +1,18 @@
 :- consult('KB2.pl').
 
-% ************************************************************ Main Logic ************************************************************
+% ************************************************************ Search ************************************************************
 
-% Define the initial state from the KB
+
+state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3)) :-
+    bottle1(Top1, Bottom1),
+    bottle2(Top2, Bottom2),
+    bottle3(Top3, Bottom3).
+
 initial_state(state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3))) :-
     bottle1(Top1, Bottom1),
     bottle2(Top2, Bottom2),
     bottle3(Top3, Bottom3).
+
 
 is_goal_bottle(bottle1(Top, Bottom)) :-
     (Top = e) 
@@ -28,185 +34,108 @@ is_goal_state(state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3
     is_goal_bottle(bottle2(Top2, Bottom2)),
     is_goal_bottle(bottle3(Top3, Bottom3)).
 
+% Search for the goal state
 search(State, Situation, S, _) :-
     is_goal_state(State),
     S = Situation.
 
 search(State, Situation, S, Visited) :-
-    \+member(State, Visited),
-    pour(From, To, State, NextState),
-    search(NextState, result(pour(From, To), Situation), S, [State | Visited]).
+    \+ member(State, Visited),
+    member(pour(X, Y), [pour(1, 2), pour(1, 3), pour(2, 1), pour(2, 3), pour(3, 1), pour(3, 2)]),
+    NextSituation = result(pour(X, Y), Situation),
+    state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3), NextSituation),
+    NextState = state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3)),
+    search(NextState, NextSituation, S, [State|Visited]).
 
 goal(S) :-
     initial_state(InitialState),
     search(InitialState, s0, S, []).
 
-% ************************************************************ Pour Operations ************************************************************
+% ************************************************************ Successor State Axiom ************************************************************
 
-% Pour from bottle1 to bottle2
-pour(1, 2, state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), B3), NextState) :-
-    (
-        % Case 1: [Top1, Bottom1] -> [e, Bottom2]
-        (
-            Top1 \= e, Top2 = e, Bottom2 \= e,
-            NextState = state(bottle1(e, Bottom1), bottle2(Top1, Bottom2), B3)
-        )
-        ;
-        % Case 2: [Top1, Bottom1] -> [e, e]
-        (
-            Top1 \= e, Top2 = e, Bottom2 = e,
-            NextState = state(bottle1(e, Bottom1), bottle2(e, Top1), B3)
-        )
-        ;
-        % Case 3: [e, Bottom1] -> [e, Bottom2]
-        (
-            Top1 = e, Bottom1 \= e, Top2 = e, Bottom2 \= e,
-            NextState = state(bottle1(e, e), bottle2(Bottom1, Bottom2), B3)
-        )
-        ;
-        % Case 4: [e, Bottom1] -> [e, e]
-        (
-            Top1 = e, Bottom1 \= e, Top2 = e, Bottom2 = e,
-            NextState = state(bottle1(e, e), bottle2(e, Bottom1), B3)
-        )
-    ).
+state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3), s0) :-
+    bottle1(Top1, Bottom1),
+    bottle2(Top2, Bottom2),
+    bottle3(Top3, Bottom3).
 
-% Pour from bottle1 to bottle3
-pour(1, 3, state(bottle1(Top1, Bottom1), B2, bottle3(Top3, Bottom3)), NextState) :-
-    (
-        % Case 1: [Top1, Bottom1] -> [e, Bottom3]
-        (
-            Top1 \= e, Top3 = e, Bottom3 \= e,
-            NextState = state(bottle1(e, Bottom1), B2, bottle3(Top1, Bottom3))
-        )
-        ;
-        % Case 2: [Top1, Bottom1] -> [e, e]
-        (
-            Top1 \= e, Top3 = e, Bottom3 = e,
-            NextState = state(bottle1(e, Bottom1), B2, bottle3(e, Top1))
-        )
-        ;
-        % Case 3: [e, Bottom1] -> [e, Bottom3]
-        (
-            Top1 = e, Bottom1 \= e, Top3 = e, Bottom3 \= e,
-            NextState = state(bottle1(e, e), B2, bottle3(Bottom1, Bottom3))
-        )
-        ;
-        % Case 4: [e, Bottom1] -> [e, e]
-        (
-            Top1 = e, Bottom1 \= e, Top3 = e, Bottom3 = e,
-            NextState = state(bottle1(e, e), B2, bottle3(e, Bottom1))
-        )
-    ).
+state(bottle1(NewTop1, NewBottom1), bottle2(NewTop2, NewBottom2), bottle3(NewTop3, NewBottom3), result(A, S)) :-
 
-% Pour from bottle2 to bottle1
-pour(2, 1, state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), B3), NextState) :-
-    (
-        % Case 1: [Top2, Bottom2] -> [e, Bottom1]
-        (
-            Top2 \= e, Top1 = e, Bottom1 \= e,
-            NextState = state(bottle1(Top2, Bottom1), bottle2(e, Bottom2), B3)
-        )
-        ;
-        % Case 2: [Top2, Bottom2] -> [e, e]
-        (
-            Top2 \= e, Top1 = e, Bottom1 = e,
-            NextState = state(bottle1(e, Top2), bottle2(e, Bottom2), B3)
-        )
-        ;
-        % Case 3: [e, Bottom2] -> [e, Bottom1]
-        (
-            Top2 = e, Bottom2 \= e, Top1 = e, Bottom1 \= e,
-            NextState = state(bottle1(Bottom2, Bottom1), bottle2(e, e), B3)
-        )
-        ;
-        % Case 4: [e, Bottom2] -> [e, e]
-        (
-            Top2 = e, Bottom2 \= e, Top1 = e, Bottom1 = e,
-            NextState = state(bottle1(e, Bottom2), bottle2(e, e), B3)
-        )
-    ).
+    state(bottle1(Top1, Bottom1), bottle2(Top2, Bottom2), bottle3(Top3, Bottom3), S),
 
-% Pour from bottle2 to bottle3
-pour(2, 3, state(B1, bottle2(Top2, Bottom2), bottle3(Top3, Bottom3)), NextState) :-
-    (
-        % Case 1: [Top2, Bottom2] -> [e, Bottom3]
-        (
-            Top2 \= e, Top3 = e, Bottom3 \= e,
-            NextState = state(B1, bottle2(e, Bottom2), bottle3(Top2, Bottom3))
-        )
-        ;
-        % Case 2: [Top2, Bottom2] -> [e, e]
-        (
-            Top2 \= e, Top3 = e, Bottom3 = e,
-            NextState = state(B1, bottle2(e, Bottom2), bottle3(e, Top2))
-        )
-        ;
-        % Case 3: [e, Bottom2] -> [e, Bottom3]
-        (
-            Top2 = e, Bottom2 \= e, Top3 = e, Bottom3 \= e,
-            NextState = state(B1, bottle2(e, e), bottle3(Bottom2, Bottom3))
-        )
-        ;
-        % Case 4: [e, Bottom2] -> [e, e]
-        (
-            Top2 = e, Bottom2 \= e, Top3 = e, Bottom3 = e,
-            NextState = state(B1, bottle2(e, e), bottle3(e, Bottom2))
-        )
-    ).
+    (A = pour(1, 2),
+        (Top1 \= e, Bottom1 \= e, Top2 = e, Bottom2 \= e ->
+            NewTop1 = e, NewBottom1 = Bottom1, NewTop2 = Top1, NewBottom2 = Bottom2
+        ; Top1 \= e, Bottom1 \= e, Top2 = e, Bottom2 = e ->
+            NewTop1 = e, NewBottom1 = Bottom1, NewTop2 = e, NewBottom2 = Top1
+        ; Top1 = e, Bottom1 \= e, Top2 = e, Bottom2 \= e ->
+            NewTop1 = e, NewBottom1 = e, NewTop2 = Bottom1, NewBottom2 = Bottom2
+        ; Top1 = e, Bottom1 \= e, Top2 = e, Bottom2 = e ->
+            NewTop1 = e, NewBottom1 = e, NewTop2 = e, NewBottom2 = Bottom2
+        ),
+        NewTop3 = Top3, NewBottom3 = Bottom3
+    ;
 
-% Pour from bottle3 to bottle1
-pour(3, 1, state(bottle1(Top1, Bottom1), B2, bottle3(Top3, Bottom3)), NextState) :-
-    (
-        % Case 1: [Top3, Bottom3] -> [e, Bottom1]
-        (
-            Top3 \= e, Top1 = e, Bottom1 \= e,
-            NextState = state(bottle1(Top3, Bottom1), B2, bottle3(e, Bottom3))
-        )
-        ;
-        % Case 2: [Top3, Bottom3] -> [e, e]
-        (
-            Top3 \= e, Top1 = e, Bottom1 = e,
-            NextState = state(bottle1(e, Top3), B2, bottle3(e, Bottom3))
-        )
-        ;
-        % Case 3: [e, Bottom3] -> [e, Bottom1]
-        (
-            Top3 = e, Bottom3 \= e, Top1 = e, Bottom1 \= e,
-            NextState = state(bottle1(Bottom3, Bottom1), B2, bottle3(e, e))
-        )
-        ;
-        % Case 4: [e, Bottom3] -> [e, e]
-        (
-            Top3 = e, Bottom3 \= e, Top1 = e, Bottom1 = e,
-            NextState = state(bottle1(e, Bottom3), B2, bottle3(e, e))
-        )
-    ).
+    A = pour(1, 3),
+        (Top1 \= e, Bottom1 \= e, Top3 = e, Bottom3 \= e ->
+            NewTop1 = e, NewBottom1 = Bottom1, NewTop3 = Top1, NewBottom3 = Bottom3
+        ; Top1 \= e, Bottom1 \= e, Top3 = e, Bottom3 = e ->
+            NewTop1 = e, NewBottom1 = Bottom1, NewTop3 = e, NewBottom3 = Top1
+        ; Top1 = e, Bottom1 \= e, Top3 = e, Bottom3 \= e ->
+            NewTop1 = e, NewBottom1 = e, NewTop3 = Bottom1, NewBottom3 = Bottom3
+        ; Top1 = e, Bottom1 \= e, Top3 = e, Bottom3 = e ->
+            NewTop1 = e, NewBottom1 = e, NewTop3 = e, NewBottom3 = Bottom1
+        ),
+        NewTop2 = Top2, NewBottom2 = Bottom2
+    ;
 
-% Pour from bottle3 to bottle2
-pour(3, 2, state(B1, bottle2(Top2, Bottom2), bottle3(Top3, Bottom3)), NextState) :-
-    (
-        % Case 1: [Top3, Bottom3] -> [e, Bottom2]
-        (
-            Top3 \= e, Top2 = e, Bottom2 \= e,
-            NextState = state(B1, bottle2(Top3, Bottom2), bottle3(e, Bottom3))
-        )
-        ;
-        % Case 2: [Top3, Bottom3] -> [e, e]
-        (
-            Top3 \= e, Top2 = e, Bottom2 = e,
-            NextState = state(B1, bottle2(e, Top3), bottle3(e, Bottom3))
-        )
-        ;
-        % Case 3: [e, Bottom3] -> [e, Bottom2]
-        (
-            Top3 = e, Bottom3 \= e, Top2 = e, Bottom2 \= e,
-            NextState = state(B1, bottle2(Bottom3, Bottom2), bottle3(e, e))
-        )
-        ;
-        % Case 4: [e, Bottom3] -> [e, e]
-        (
-            Top3 = e, Bottom3 \= e, Top2 = e, Bottom2 = e,
-            NextState = state(B1, bottle2(e, Bottom3), bottle3(e, e))
-        )
+    A = pour(2, 1),
+        (Top2 \= e, Bottom2 \= e, Top1 = e, Bottom1 \= e ->
+            NewTop2 = e, NewBottom2 = Bottom2, NewTop1 = Top2, NewBottom1 = Bottom1
+        ; Top2 \= e, Bottom2 \= e, Top1 = e, Bottom1 = e ->
+            NewTop2 = e, NewBottom2 = Bottom2, NewTop1 = e, NewBottom1 = Top2
+        ; Top2 = e, Bottom2 \= e, Top1 = e, Bottom1 \= e ->
+            NewTop2 = e, NewBottom2 = e, NewTop1 = Bottom2, NewBottom1 = Bottom1
+        ; Top2 = e, Bottom2 \= e, Top1 = e, Bottom1 = e ->
+            NewTop2 = e, NewBottom2 = e, NewTop1 = e, NewBottom1 = Bottom2
+        ),
+        NewTop3 = Top3, NewBottom3 = Bottom3
+    ;
+
+    A = pour(2, 3),
+        (Top2 \= e, Bottom2 \= e, Top3 = e, Bottom3 \= e ->
+            NewTop2 = e, NewBottom2 = Bottom2, NewTop3 = Top2, NewBottom3 = Bottom3
+        ; Top2 \= e, Bottom2 \= e, Top3 = e, Bottom3 = e ->
+            NewTop2 = e, NewBottom2 = Bottom2, NewTop3 = e, NewBottom3 = Top2
+        ; Top2 = e, Bottom2 \= e, Top3 = e, Bottom3 \= e ->
+            NewTop2 = e, NewBottom2 = e, NewTop3 = Bottom2, NewBottom3 = Bottom3
+        ; Top2 = e, Bottom2 \= e, Top3 = e, Bottom3 = e ->
+            NewTop2 = e, NewBottom2 = e, NewTop3 = e, NewBottom3 = Bottom2
+        ),
+        NewTop1 = Top1, NewBottom1 = Bottom1
+    ;
+
+    A = pour(3, 1),
+        (Top3 \= e, Bottom3 \= e, Top1 = e, Bottom1 \= e ->
+            NewTop3 = e, NewBottom3 = Bottom3, NewTop1 = Top3, NewBottom1 = Bottom1
+        ; Top3 \= e, Bottom3 \= e, Top1 = e, Bottom1 = e ->
+            NewTop3 = e, NewBottom3 = Bottom3, NewTop1 = e, NewBottom1 = Top3
+        ; Top3 = e, Bottom3 \= e, Top1 = e, Bottom1 \= e ->
+            NewTop3 = e, NewBottom3 = e, NewTop1 = Bottom3, NewBottom1 = Bottom1
+        ; Top3 = e, Bottom3 \= e, Top1 = e, Bottom1 = e ->
+            NewTop3 = e, NewBottom3 = e, NewTop1 = e, NewBottom1 = Bottom3
+        ),
+        NewTop2 = Top2, NewBottom2 = Bottom2
+    ;
+
+    A = pour(3, 2),
+        (Top3 \= e, Bottom3 \= e, Top2 = e, Bottom2 \= e ->
+            NewTop3 = e, NewBottom3 = Bottom3, NewTop2 = Top3, NewBottom2 = Bottom2
+        ; Top3 \= e, Bottom3 \= e, Top2 = e, Bottom2 = e ->
+            NewTop3 = e, NewBottom3 = Bottom3, NewTop2 = e, NewBottom2 = Top3
+        ; Top3 = e, Bottom3 \= e, Top2 = e, Bottom2 \= e ->
+            NewTop3 = e, NewBottom3 = e, NewTop2 = Bottom3, NewBottom2 = Bottom2
+        ; Top3 = e, Bottom3 \= e, Top2 = e, Bottom2 = e ->
+            NewTop3 = e, NewBottom3 = e, NewTop2 = e, NewBottom2 = Bottom3
+        ),
+        NewTop1 = Top1, NewBottom1 = Bottom1
     ).
